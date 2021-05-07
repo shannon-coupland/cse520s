55ab21b58bfa136db1c18d8e1fd88b587743d712
shannon-coupland <35540987+shannon-coupland@users.noreply.github.com>
2021-05-07 14:37:26 -0500

    Update buzzer_end.py

--- pi_code/buzzer_end.py
+++ pi_code/buzzer_end.py
@@ -1,13 +1,16 @@
 #Libraries
 import RPi.GPIO as GPIO
 from time import sleep
-#Disable warnings (optional)
+
+#Disable warnings
 GPIO.setwarnings(False)
+
 #Select GPIO mode
 GPIO.setmode(GPIO.BCM)
+
 #Set buzzer - pin 23 as output
-buzzer=23 
+buzzer = 23 
 GPIO.setup(buzzer,GPIO.OUT)
 
-#Run buzzer
+#Stop buzzer
 GPIO.output(buzzer,GPIO.LOW)
